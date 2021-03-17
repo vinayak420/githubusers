3e6d1d768ab43a36dd11d0ec464ee4f5c6ef9121
Vinayak Mishra <vinayakmishra98@yahoo.com>
2021-03-17 16:26:53 +0530

    commit before refactor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
 import "./App.css";
+import ReactDOM from "react"
 import Navbar from "./Components/Navbar";
 import AllUsers from "./Components/AllUsers";
 import User from "./Components/User";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className='App'>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={AllUsers} />
-          <Route path='/:username' exact component={User} />
-        </Switch>
-      </Router>
-    </div>
-  );
-}
+class App extends ReactDOM.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      allData: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch("https://api.github.com/users")
+      .then((resposnse) => resposnse.json())
+      .then((data) =>
+        this.setState({
+          allData: data,
+          isLoading: false,
+        })
+      );
+  }
+
+    sortData =(param)=>{
+    this.setState({
+      allData: data.sort((a,b) =>  a[param] - b[param])
+      })
+      console.log(this.state)
+    }
+
+  render(){
+        if (this.state.isLoading) {
+        return <h1>Loading...</h1>;
+      } else {
+      return (
+        <div className='App'>
+          <Router>
+            <Navbar />
+            <Switch>
+              <Route path='/' exact render={(props) => (<AllUsers {...props} data={this.state.allData} sortData = {this.sortData} />)} />
+              <Route path='/:username' exact component={User} />
+            </Switch>
+          </Router>
+        </div>
+      );
+    }
+  }
+}
 export default App;
--- src/Components/AllUsers.js
+++ src/Components/AllUsers.js
@@ -6,43 +6,18 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Result from "./Result";
 
 class AllUsers extends ReactDOM.Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoading: false,
-      allData: [],
-      search: "",
-    };
-  }
 
-  componentDidMount() {
-    this.setState({ isLoading: true });
-    fetch("https://api.github.com/users")
-      .then((resposnse) => resposnse.json())
-      .then((data) =>
-        this.setState({
-          allData: data,
-          isLoading: false,
-        })
-      );
-  }
 
-  handleChange = (event) => {
-    this.setState({
-      search: event.target.value,
-    });
-  };
+
 
   render() {
-    const users = this.state.allData.map((data) => (
+    const users = this.props.data.map((data) => (
       <Link to={`/${data.login}`}>
         <Alldata key={data.id} username={data.login} avatar={data.avatar_url} />
       </Link>
     ));
 
-    if (this.state.isLoading) {
-      return <h1>Loading...</h1>;
-    } else {
+ 
       return (
         <div>
           <form>
@@ -54,12 +29,12 @@ class AllUsers extends ReactDOM.Component {
             ></input>
             <button>Search</button>
           </form>
-          {/* <Result data={this.state.search} /> */}
+          <button onClick = {()=> this.props.sortData("followers")}> Sort by followers</button>
           {users}
         </div>
       );
     }
   }
-}
+// }
 
 export default AllUsers;
